28e44d6f80d1d6b7482506f801664420369b7c97
MattiaAccatiSheard <mattiahtsheard@gmail.com>
2023-02-18 14:57:30 +0100

    .prove con millis

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
+const mSecondEl = document.getElementById("msecond")
 const titleEl = document.getElementById("title");
+
+const ZdaysEl = document.getElementById("z-days");
+const ZhoursEl = document.getElementById("z-hours");
+const ZminutesEl = document.getElementById("z-minutes");
+const ZsecondsEl = document.getElementById("z-seconds");
+const ZmSecondEl = document.getElementById("z-msecond")
+const ZtitleEl = document.getElementById("title");
 
 // ESEMPIO 1: PASTA
 // titleEl.innerHTML = "Butta la pasta!";
@@ -22,23 +30,50 @@ const titleEl = document.getElementById("title");
 // }
 
 // ESEMPIO 2: CRONOMETRO
+titleEl.innerHTML = "Cronometro";
+let startTime = performance.now();
+let totalSeconds = 0;
+const clockCronometro = setInterval(stampaTempoTrascorso, 1000);
+
+stampaTempoTrascorso();
+ZstampaTempoTrascorso();
+function stampaTempoTrascorso() {
+    ++totalSeconds;
+
+    const seconds = totalSeconds % 60;
+    const minutes = parseInt((totalSeconds / 60) % 60);
+    const hours = parseInt((totalSeconds / 60 / 60) % 24);
+    const days = parseInt((totalSeconds / 60 / 60 / 24));
+
+    secondsEl.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
+    minutesEl.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
+    hoursEl.innerHTML = (hours < 10) ? "0" + hours : hours;
+    daysEl.innerHTML = (days < 10) ? "0" + days : days;
+}
+
+
+// 2: CRONOMETRO CON MILLISECONDI
 // titleEl.innerHTML = "Cronometro";
-// let totalSeconds = 100000;
-// const clockCronometro = setInterval(stampaTempoTrascorso, 1000);
-// stampaTempoTrascorso();
-// function stampaTempoTrascorso() {
-// 	++totalSeconds;
+// let totalSeconds = 0;
+const ZclockCronometro = setInterval(ZstampaTempoTrascorso, 10);
+function ZstampaTempoTrascorso() {
+    let currentTime = performance.now();
+    let startTime = startTime || currentTime;
+    let elapsedTime = currentTime - startTime;
 
-// 	const seconds = totalSeconds % 60;
-// 	const minutes = parseInt((totalSeconds / 60) % 60);
-// 	const hours = parseInt((totalSeconds / 60 / 60) % 24);
-// 	const days = parseInt((totalSeconds / 60 / 60 / 24));
 
-// 	secondsEl.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
-// 	minutesEl.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
-// 	hoursEl.innerHTML = (hours < 10) ? "0" + hours : hours;
-// 	daysEl.innerHTML = (days < 10) ? "0" + days : days;
-// }
+    let seconds = parseInt((elapsedTime / 1000) % 60);
+    let minutes = parseInt((elapsedTime / 1000 / 60) % 60);
+    let hours = parseInt((elapsedTime / 1000 / 60 / 60) % 24);
+    let days = parseInt((elapsedTime / 1000 / 60 / 60 / 24));
+    let milliseconds = parseInt(elapsedTime % 1000);
+
+    ZsecondsEl.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
+    ZminutesEl.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
+    ZhoursEl.innerHTML = (hours < 10) ? "0" + hours : hours;
+    ZdaysEl.innerHTML = (days < 10) ? "0" + days : days;
+    ZmSecondEl.innerHTML = (milliseconds < 10) ? "00" + milliseconds : (milliseconds < 100) ? "0" + milliseconds : milliseconds;
+}
 
 // ESEMPIO 3: OROLOGIO
 // stampaOraAttuale()
@@ -67,31 +102,31 @@ const titleEl = document.getElementById("title");
 
 
 // countdown
-const countDown = new Date("2023-02-04 9:30 ").getTime();
+// const countDown = new Date("2023-02-14 11:30 ").getTime();
 
-const clockDown = setInterval(conteggio, 1000);
+// const clockDown = setInterval(conteggio, 1000);
 
-function conteggio() {
-    const now = new Date().getTime();
+// function conteggio() {
+//     const now = new Date().getTime();
 
-    const differenza = countDown - now;
+//     const differenza = countDown - now;
 
-    const days = Math.floor(differenza / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((differenza % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((differenza % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((differenza % (1000 * 60)) / 1000);
+//     const days = Math.floor(differenza / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((differenza % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const minutes = Math.floor((differenza % (1000 * 60 * 60)) / (1000 * 60));
+//     const seconds = Math.floor((differenza % (1000 * 60)) / 1000);
 
-    secondsEl.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
-    minutesEl.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
-    hoursEl.innerHTML = (hours < 10) ? "0" + hours : hours;
-    daysEl.innerHTML = (days < 10) ? "0" + days : days;
+//     secondsEl.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
+//     minutesEl.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
+//     hoursEl.innerHTML = (hours < 10) ? "0" + hours : hours;
+//     daysEl.innerHTML = (days < 10) ? "0" + days : days;
 
-    if (differenza < 0) {
-        clearInterval(clockDown);
-        confetti({
-            particleCount: 100,
-            spread: 160
-        });
-    }
+//     if (differenza < 0) {
+//         clearInterval(clockDown);
+//         confetti({
+//             particleCount: 100,
+//             spread: 160
+//         });
+//     }
 
-}
+// }
